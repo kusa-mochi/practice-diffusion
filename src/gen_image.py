0c3e6b474a142a11bdb0e993439d76ddc6af9344
kusa-mochi <whoatemyapplepie@gmail.com>
2025-09-21 21:09:22 +0900

    change an output filename.

--- src/gen_image.py
+++ src/gen_image.py
@@ -1,4 +1,5 @@
 import sys
+import datetime
 from diffusers import StableDiffusionPipeline, EulerDiscreteScheduler
 
 args = sys.argv
@@ -13,4 +14,10 @@ pipe = StableDiffusionPipeline.from_pretrained(model_id, scheduler=scheduler)
 pipe = pipe.to("cpu")
 prompt = args[1]
 image = pipe(prompt).images[0]
-image.save(prompt.replace(' ', '_') + ".png")
+# image.save(prompt.replace(' ', '_') + ".png")
+
+# 現在時刻を取得してファイル名にする
+now = datetime.datetime.now()
+filename = now.strftime("%Y%m%d_%H%M%S") + ".png"
+image.save(filename)
+print(f"Saved image: {filename}")
